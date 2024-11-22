c235ba361ab4eb4a39a7fff7cd626dcfc741699c
Andrei Visoiu <a.visoiu@student.maastrichtuniversity.nl>
2024-11-22 12:02:06 +0100

    Updated the Base component.

--- com.unity.ml-agents/Runtime/Sensors/RayPerceptionSensorComponentBase.cs
+++ com.unity.ml-agents/Runtime/Sensors/RayPerceptionSensorComponentBase.cs
@@ -329,7 +329,7 @@ namespace Unity.MLAgents.Sensors
 
         void OnDrawGizmosSelected()
         {
-            if (m_RaySensor?.RayPerceptionOutput?.RayOutputs != null)
+            if (m_RaySensor?.RayPerceptionOutput?.RayOutputs != null || m_RaySensor?.RayPerceptionOutput?.RayOutputs.Length > 0)
             {
                 // If we have cached debug info from the sensor, draw that.
                 // Draw "old" observations in a lighter color.
