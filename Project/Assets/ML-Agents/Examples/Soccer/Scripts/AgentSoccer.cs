bbe7fed7d5be49d2f353da3c005767645f2a10ee
Yusuf Serhat Ozkan <y.serhatozkan@gmail.com>
2024-11-30 12:30:34 +0100

    increased the reward for on collision

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/AgentSoccer.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/AgentSoccer.cs
@@ -298,7 +298,7 @@ public class AgentSoccer : Agent
         }
         if (c.gameObject.CompareTag("ball"))
         {
-            AddReward(0.2f * m_BallTouch);
+            AddReward(0.8f * m_BallTouch);
             var dir = c.contacts[0].point - transform.position;
             dir = dir.normalized;
             c.gameObject.GetComponent<Rigidbody>().AddForce(dir * force);
